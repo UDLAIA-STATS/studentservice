ee9cee76b1834d9c9a0fa36328fe2b58dd672735
SebastianG03 <dasedev24@gmail.com>
2026-01-21 19:21:52 -0500

    Secret key setted

--- studentservice/settings.py
+++ studentservice/settings.py
@@ -18,7 +18,7 @@ rs.default_port = 8030
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vqldfqjpb%@(r1=j3bp$@4r&pojyr04(%x62nqjn@wg@bqu$gv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
